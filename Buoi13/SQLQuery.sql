be13a6e19eeb5b740482fe8d12d026928df2afd8
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-04-24 16:14:40 +0700

    LAM THEM

--- Buoi13/SQLQuery.sql
+++ Buoi13/SQLQuery.sql
@@ -39,13 +39,44 @@ END
 
 SELECT DISTINCT T2.Country,[dbo].NumberOfCustomer(T2.Country) AS 'NumberOfCustomer'   FROM [dbo].Customers AS T2 
 --
-SELECT ProductID,SUM(Quantity) AS TotalSelling
-FROM [dbo].[Order Details] 
-GROUP BY ProductID
+SELECT T1.ProductID,T2.ProductName,T2.UnitPrice,SUM(Quantity) AS TotalSelling
+FROM [dbo].[Order Details] T1 JOIN [dbo].[Products]  AS T2 ON T1.ProductID = T2.ProductID
+WHERE T1.ProductID=75
+GROUP BY T1.ProductID,T2.ProductName,T2.UnitPrice
 HAVING SUM(Quantity)>1000
 
-SELECT ProductID ,SUM(Quantity) FROM [dbo].[Order Details] 
-GROUP BY ProductID 
+ALTER FUNCTION sumsale(@Product int)
+RETURNS TABLE
+AS
+RETURN(
+	SELECT T1.ProductID,T2.ProductName,T2.UnitPrice,SUM(Quantity) AS TotalSelling
+	FROM [dbo].[Order Details] T1 JOIN [dbo].[Products]  AS T2 ON T1.ProductID = T2.ProductID
+	WHERE T1.ProductID= @Product
+	GROUP BY T1.ProductID,T2.ProductName,T2.UnitPrice
+	HAVING SUM(Quantity)>1000
+)
+--Check sumsale function
+SELECT * FROM sumsale(75)
 
+--4.	To calculate salary for salesman, manager would like to know total amount of money each salesman have been sold. .
+--Write a function to display information as follow :
+--EmployeeID, LastName, FirstName, TotalAmount 
 
+SELECT T1.EmployeeID,T2.FirstName,T2.LastName,SUM([dbo].TotalAmount(T1.OrderID)) AS TotalAmount
+FROM Orders AS T1
+	 JOIN [dbo].Employees T2 ON T1.EmployeeID = T2.EmployeeID
+WHERE T1.EmployeeID =1 
+GROUP BY T1.EmployeeID,T2.FirstName,T2.LastName
 
+CREATE FUNCTION SalemanCal(@EmployeeID int)
+RETURNS TABLE
+AS
+RETURN(
+		SELECT T1.EmployeeID,T2.FirstName,T2.LastName,SUM([dbo].TotalAmount(T1.OrderID)) AS TotalAmount
+	FROM Orders AS T1
+		 JOIN [dbo].Employees T2 ON T1.EmployeeID = T2.EmployeeID
+	WHERE T1.EmployeeID = @EmployeeID
+	GROUP BY T1.EmployeeID,T2.FirstName,T2.LastName
+)
+--TEST
+SELECT * FROM [dbo].SalemanCal(1)
