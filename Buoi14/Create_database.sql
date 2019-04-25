2894466947b335db874252627417b8deb506233d
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-04-25 20:39:07 +0700

    sadasdas

--- Buoi14/Create_database.sql
+++ Buoi14/Create_database.sql
@@ -1,2 +1,17 @@
 CREATE DATABASE HANGKHONG
+
 USE HANGKHONG
+-- Create table Employees
+DROP TABLE IF EXISTS [dbo].CHUYENBAY 
+CREATE TABLE CHUYENBAY 
+(
+	MaCB char(5),
+	GaDi varchar(50),
+	GaDen varchar(50),
+	DoDai int,
+	GioDi time,
+	GioDen time,
+	ChiPhi int,
+	CONSTRAINT PK_MaCB
+    PRIMARY KEY(MaCB)
+)
